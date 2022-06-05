c27c977aa7c5967f5e15cdf782b3fa770dbb425d
ilhan <canilhn1@gmail.com>
2022-06-05 23:13:23 +0300

    Task1'in koşulları yerine getirildi. Task2'in koşulları yerine getirildi.(FilterApiImpl classı düzenlendi.) Bonus task  gerçekleştirildi.Email class içierisndeki DRY giderildi.

--- src/main/java/com/kodgemisi/usermanagement/UserService.java
+++ src/main/java/com/kodgemisi/usermanagement/UserService.java
@@ -37,8 +37,10 @@ public class UserService implements UserApi {
 	{
 		Optional<User> userUpdated=	userDao.find(user.getId());
 		userUpdated.get().setProfile(user.getProfile());
-
-
+		userUpdated.get().setAge(user.getAge());
+		userUpdated.get().setId(user.getId());
+		userUpdated.get().setRole(user.getRole());
+		userUpdated.get().setCreatedAt(user.getCreatedAt());
 		return userDao.update(userUpdated.get());
 	}
 
